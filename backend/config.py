51052eb5145d95551142e1b0c747361bcbbf1ee7
doanbarin <doanbarin@gmail.com>
2026-05-09 10:20:09 +0700

    first commit

---
+++ backend/config.py
@@ -0,0 +1,31 @@
+import pyodbc
+import mysql.connector
+
+def get_sqlserver_connection():
+    try:
+        conn = pyodbc.connect(
+            "DRIVER={ODBC Driver 17 for SQL Server};"
+            "SERVER=localhost;"
+            "DATABASE=HUMAN_2025;"
+            "UID=sa;"
+            "PWD=123456;",
+            timeout=5
+        )
+        return conn
+    except Exception as e:
+        print("Lỗi kết nối SQL Server:", str(e))
+        raise
+
+def get_mysql_connection():
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="123456",
+            database="payroll_2026",
+            autocommit=False
+        )
+        return conn
+    except Exception as e:
+        print("Lỗi kết nối MySQL:", str(e))
+        raise
